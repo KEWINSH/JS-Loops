3bbf90acd6dd1c96a2ca4f3547bd3a4367b97f2e
KEWINSH <93644595+KEWINSH@users.noreply.github.com>
2021-12-06 14:06:09 +0100

    Add files via upload

---
+++ Python-JavaScript/Python/JavaLoops.js
@@ -0,0 +1,12 @@
+// Oppgave 1
+
+var nummer = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+var partall = nummer.filter(function(item) {
+   return (item % 2 == 0);
+});
+
+document.write(partall);
+
+// Oppgave 2
+
