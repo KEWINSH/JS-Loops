0e7a4fb31c0c630b2227182a3d42255a7322f124
KEWINSH <93644595+KEWINSH@users.noreply.github.com>
2021-12-08 10:04:54 +0100

    Oppdatert og Ferdiggjort oppgave

---
+++ Java For Loops.js
@@ -0,0 +1,48 @@
+// Oppgave 1
+
+//for(var i=0; i<50; i++) {
+   //if(i%2==0)
+   //console.log(i);
+//}
+
+
+
+// Oppgave 2
+
+//for(var i=0; i<=100; i+=1) {
+   //console.log(i);
+//}
+
+
+// Oppgave 3
+
+// Oppgave 4
+
+// Oppgave 5
+
+var a = prompt("Skrivr noe:")
+
+for (var i = 6; i > 0; i--){
+}
+document.write("<h"+i+">"+a+"</h")
+console.log(i);
+
+
+// Oppgave 6
+
+/*var høyde = window.promt("Skriv inn Høyden")
+
+var bredde = window.promt("Skriv inn Bredde")
+
+for (var i = 1; i <= høyde; i++){
+   for (var j = 1; j <= bredde; j++) {
+      if (i == 1 || i == høyde) document.write("*")
+      else if (j == 1 || j == bredde) document.write("*")
+      else {
+         document.write("&nbsp;")
+         document.write("&nbsp;")
+      }
+   }
+   document.write("<br>")
+}
+*/
